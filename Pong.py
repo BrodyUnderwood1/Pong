32d0fc0d0a649a67ef8a3deea8aea0c00dc3f352
tbunde01 <tbunde01@cardmail.louisville.edu>
2018-04-14 21:48:01 -0400

    Updated Code
    Added Ball, Players, & Player Scores
    Added Hard Coded Movement of player objects to demonstrate movement.   Will be removed.

--- Pong.py
+++ Pong.py
@@ -8,16 +8,73 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode(SIZE)
 background_top = pygame.image.load(BTI).convert()
 background_bot = pygame.image.load(BBI).convert()
+# Select the font to use, size, bold, italics
+font = pygame.font.SysFont('Calibri', 25, True, False)
+players_list = pygame.sprite.Group()
+all_sprites_list = pygame.sprite.Group()
 
+#Pong ball class
+class Ball(pygame.sprite.Sprite):
+	
+	def __init__(self):
+		super().__init__()
+		self.image = pygame.image.load(BI).convert()
+		self.image.set_colorkey(WHITE)
+		self.rect = self.image.get_rect()
+
+
+class players(pygame.sprite.Sprite):
+
+	def __init__(self, image):
+		super().__init__()
+		self.image = pygame.image.load(image).convert()
+		self.image.set_colorkey(WHITE)
+		self.rect = self.image.get_rect()
+		self.score=0
+
+
+
+
+p1 = players(P1I)
+p1.rect.x = P1XP
+p1.rect.y = 250
+players_list.add(p1)
+all_sprites_list.add(p1)
+p2 = players(P2I)
+p2.rect.x = P2XP
+p2.rect.y = 250
+players_list.add(p2)
+all_sprites_list.add(p2)
+
+ball = Ball()
+ball.rect.x = 345
+ball.rect.y = 295
+all_sprites_list.add(ball)
+
+theta = 0
+
+def drawBackground():
+	screen.fill(WHITE)
+	screen.blit(background_top, BTP)
+	screen.blit(background_bot, BBP)
+
+def drawScores():
+	text = font.render(SCORE1 + str(p1.score), True, GREEN)
+	screen.blit(text, [10, 10])
+	text = font.render(SCORE2 + str(p2.score), True, GREEN)
+	screen.blit(text, [570,10])
 
 while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
-	screen.blit(background_top, BTP)
-	screen.blit(background_bot, BBP)
-
+	p1.rect.y = 250 + math.sin(theta)*160 #just to show animation of sprites
+	p2.rect.y = 250 - math.sin(theta)*160
+	drawBackground()
+	drawScores()
+	all_sprites_list.draw(screen)
 	pygame.display.flip()
 	clock.tick(60)
+	theta += .01
 
 pygame.quit()
--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 #Setup
 
+#score text
+SCORE1 = "Player 1: "
+SCORE2 = "Player 2: "
 #colors
+BC1 = ( 0, 192, 255 )
+BC2 = ( 255, 63, 0 )
 BLACK = ( 0, 0, 0)
 WHITE = (255, 255, 255)
-RED = (255, 0, 0)
+RED = ( 255, 0, 0 )
+GREEN = ( 0, 255, 0 )
+BLUE = ( 0, 0, 255 )
 
 #screen size
 SIZE = [700,600]
@@ -13,10 +20,11 @@ BTI = "Images\Background_Top.png" #path to Background Top Image
 BBI = "Images\Background_Bottom.png" #path to Background Bot Image
 P1I = "Images\player1.png" #path to player 1 sprite image
 P2I = "Images\player2.png"	#parth to player 2 sprite image
+BI = "Images\\ball.png"
 
 #player positions
 P1XP = 20 #x position for player 1
-P2XP = 680 #x position for player 2
+P2XP = 640 #x position for player 2
 
 #background positions
 BTP = [0,0] #background top position
